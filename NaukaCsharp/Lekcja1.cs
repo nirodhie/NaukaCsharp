c958768ed9b5e10cb6071cc949f2c74b769ebe81
nirodhie <lteska2@gmail.com>
2018-11-07 07:50:28 +0100

    Add project files.

---
+++ NaukaCsharp/Lekcja1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaukaCsharp
+{
+    class Lekcja1
+    {
+        static void Main(string[] args) //zawsze tylko jedna metoda o nazwie main, ona uruchamia program
+        {
+            Console.WriteLine("Naciśnij dowolny klawisz...");
+            Console.ReadKey();
+        }
+    }
+}
