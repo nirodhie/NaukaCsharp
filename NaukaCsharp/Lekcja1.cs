b8394eb2da415867a8939ec2a981edcbf8167756
nirodhie <lteska2@gmail.com>
2018-11-07 17:30:47 +0100

    ,

--- NaukaCsharp/Lekcja1.cs
+++ NaukaCsharp/Lekcja1.cs
@@ -3,14 +3,14 @@ using System.Text; //pozwala ustawić kodowanie dzięki czemu w konsoli będą w
 
 namespace NaukaCsharp //w tej przestrzeni obowiązują nazwy zmiennych i klas, poza nią można nadawać takie same nazwy
 {
-    class Lekcja1 //klasy są mniejsze od przestrzeni nazw
+    internal class Lekcja1 //klasy są mniejsze od przestrzeni nazw
     {
-        static void Main(string[] args) //zawsze tylko jedna metoda o nazwie main, ona uruchamia program
+        private static void Main(string[] args) //zawsze tylko jedna metoda o nazwie main, ona uruchamia program
         {//metody grupują pojedyńcze instrukcje
 
-            var zmienna = uint.MaxValue; //zmienna powinna się zaczynać z małej litery, aby odróżnić od metod i klas, nie może być spacji w nazwie
-                             //nie może być słów kluczowych typu int,void będących poleceniami języka. Każda zmienna ma swoją komórkę w pamięci
-            var zmienna_tekstowa = "jakiś tekst";
+            uint zmienna = uint.MaxValue; //zmienna powinna się zaczynać z małej litery, aby odróżnić od metod i klas, nie może być spacji w nazwie
+                                          //nie może być słów kluczowych typu int,void będących poleceniami języka. Każda zmienna ma swoją komórkę w pamięci
+            string zmienna_tekstowa = "jakiś tekst";
 
             //wartości całkowite
             byte zmienna_bitowa = byte.MaxValue; //od 0 do 255
@@ -30,25 +30,29 @@ namespace NaukaCsharp //w tej przestrzeni obowiązują nazwy zmiennych i klas, p
             bool logiczna = true; //albo false, zmienna logiczna; prawda lub fałsz
             bool obliczanie1 = 1 > 2; //wyjdzie False bo 1 nie jest większe od 2; czy jenda wartość jest większa od drugiej
 
-            const string Staua = "niezmienna wartosc"; //jesli chcemy zeby wartosc zmiennej nigdy się nie zmieniła, od razu trzeba nadać wartość
+            const string Staua = "niezmienna wartość"; //jesli chcemy zeby wartosc zmiennej nigdy się nie zmieniła, od razu trzeba nadać wartość
 
             Console.WriteLine("C# rulz"); //instrukcja
             Console.OutputEncoding = Encoding.GetEncoding("Windows-1250"); //ustawia kodowanie tak żeby wyświetlało polskie znaki w konsoli
-            Console.WriteLine(zmienna_tekstowa+"\n Maksymalna długość int to "+zmienna); //instrukcja
-            Console.WriteLine("\n Maksymalna długość "+ zmienna_bitowa.GetTypeCode() +" " + zmienna_bitowa);// "\n" oznacza przejście do kolejnej linii
+            Console.WriteLine(zmienna_tekstowa + "\n Maksymalna długość int to " + zmienna); //instrukcja
+            Console.WriteLine("\n Maksymalna długość " + zmienna_bitowa.GetTypeCode() + " " + zmienna_bitowa);// "\n" oznacza przejście do kolejnej linii
             Console.WriteLine("\n Maksymalna długość " + zmienna_short.GetTypeCode() + " to " + zmienna_short);
             Console.WriteLine("\n Maksymalna długość " + zmienna_long.GetTypeCode() + " to " + zmienna_long);
             Console.WriteLine(obliczanie1);
 
-            // komentarz jednolinijkowy
-            
+            //komentarz jednolinijkowy
+
             /*
-             * komentarz wielolinijkowy
+             * komentarz wielolinijkowy, w każdej linijce musi być *
              */
 
-
-
-
+            //CTRL+K a potem CTRL+D czyści kod
+            //CTRL+K a potem CTRL+C zmienia zaznaczone linijki w komentarze, CTRL+K a potem CTRL+U odkomentowuje
+            //F5 kompiluje
+            //snippety - jest ich dużo, trzeba poszukać innych fajnych
+            //cw TAB TAB - console.writeline
+            //for TAB TAB tworzy pętle for
+            
 
             Console.ReadKey(); //żeby okno nie zniknęło od razu po wykonaniu poleceń
         }
